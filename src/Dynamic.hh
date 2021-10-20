71f83fe9012e4cbace5ee26a0a6866392cc319ef
Olaf Willocx <olaf@usa.com>
2021-10-20 19:35:11 +0200

    simplifications, style, and comments

--- src/Dynamic.hh
+++ src/Dynamic.hh
@@ -7,25 +7,40 @@ namespace Irulan
 {   namespace Dynamic
     {
 
+// Dynamic 'Array' has runtime size and heap allocated data.
+
 template <typename ...Properties>
 struct Array : Base<Properties...>
 {
 
 public:
+
     using Base<Properties...>::layout,
           Base<Properties...>::axis,
           typename Base<Properties...>::size_type,
           typename Base<Properties...>::value_type;
     static constexpr size_t order {Base<Properties...>::dims[0]};
-private:
-    using Base<Properties...>::combinations;
-    using deep_init_list = typename Base<Properties...>::template DeepInitList<order, size_type>::type;
+
+
 
 private:
+
+    using Base<Properties...>::combinations;
+    using DeepInitList = typename Base<Properties...>::template DeepInitList<order, size_type>::type;
+
+
+
+private:
+
     size_type dims[order];
     value_type *data;
 
+
+
 private:
+
+    // Calculate the data size, which depends on 'Layout' and 'dims'.
+
     template <typename ...Dims>
     static size_t size(Dims... dims) noexcept
     {   if constexpr (layout == conventional)
@@ -34,7 +49,13 @@ private:
             return combinations(order + [](auto a, auto... b){ return a; }(dims...) - 1, order);
     }
 
+
+
 private:
+
+    // Calculates 1D memory index based on nD 'Array' index where n is 'order'.
+    // Currently only implemented for 'Layout<conventional>'.
+
     template <size_t level, typename I, typename ...J>
     auto index(I i, J... j) const noexcept
     {   if constexpr (sizeof...(j) == 0)
@@ -42,6 +63,10 @@ private:
         else
             return index<level + 1>(j...) * (*this)[level] + i;
     }
+
+    // Calculates 1D memory index based on nD 'Array' index. Behavior is 'Axis' property dependent if n < 'order'.
+    // Currently only implemented for 'Layout<conventional>'.
+
     template <size_t level, typename ...I>
     auto index_offset(I... i) const noexcept
     {   if constexpr (level == 0)
@@ -50,7 +75,12 @@ private:
             return index_offset<level - 1>(0, i...);
     }
 
+
+
 public:
+
+    // Malloc is used for data memory allocation due to need for future CUDA compatibility.
+
     template <typename ...Dims>
     Array(Dims... dims)
         : dims {static_cast<size_type>(dims)...},
@@ -59,7 +89,12 @@ public:
             throw std::bad_alloc {};
     }
 
+
+
 public:
+
+    // Dimension operator.
+
     template <typename I, std::enable_if_t<std::is_integral_v<I>>* = nullptr>
     const size_type& operator[](I i) const noexcept
     {   return dims[i];
@@ -69,29 +104,50 @@ public:
     {   return dims[i];
     }
 
+
+
 public:
+
+    // Raw data access.
+
     value_type *operator()() noexcept
     {   return data;
     }
 
+
+
 public:
+
+    // Indexing.
+    // Currently only implemented for 'Layout<conventional>'.
+
     template <typename ...I, std::enable_if_t<(std::is_integral_v<I> && ...)>* = nullptr>
     value_type& operator()(I... i) noexcept
     {   return (*this)()[index_offset<order - sizeof...(i)>(i...)];
     }
+
     template <typename ...I, std::enable_if_t<(std::is_integral_v<I> && ...)>* = nullptr>
     const value_type& operator()(I... i) const noexcept
     {   return (*this)()[index_offset<order - sizeof...(i)>(i...)];
     }
 
+
+
 public:
+
+    // Assignment via a value will set all data to this value.
+
     Array& operator=(const value_type& value) noexcept
-    {   for (size_type i {}; i < (*this)[order - 1]; i++)
+    {   for (size_type i = 0; i < (*this)[order - 1]; i++)
             (*this)(i) = value;
         return *this;
     }
-    Array& operator=(const deep_init_list& list) noexcept
-    {   for (size_type i {}; i < list.size(); i++)
+
+    // Assignment via a 'DeepInitList' does not require said list to be full, and may use the previously defined value
+    // assignment operator (in case the list's order is less than 'order').
+
+    Array& operator=(const DeepInitList& list) noexcept
+    {   for (size_type i = 0; i < list.size(); i++)
             (*this)(i) = list.begin()[i];
         return *this;
     }
--- src/Hybrid.hh
+++ src/Hybrid.hh
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdlib>
 #include <new>
 #include "Static.hh"
 
@@ -6,28 +7,44 @@ namespace Irulan
 {   namespace Hybrid
     {
 
+// Hybrid 'Array' has compile time size and heap allocated data.
+
 template <typename ...Properties>
 struct Array : Base<Properties...>
 {
 
 public:
+
     using Base<Properties...>::layout,
           Base<Properties...>::axis,
           typename Base<Properties...>::size_type,
           typename Base<Properties...>::value_type;
     static constexpr size_t order {Base<Properties...>::dims.size()};
-private:
-    using deep_init_list = typename Base<Properties...>::template DeepInitList<order, size_type>::type;
+
+
 
 private:
+
+    using DeepInitList = typename Base<Properties...>::template DeepInitList<order, size_type>::type;
+
+
+
+private:
+
     Static::Array<Properties...> *data;
 
+
+
 public:
+
+    // Malloc is used for data memory allocation due to need for future CUDA compatibility.
+
     Array()
         : data {static_cast<decltype(data)>(malloc(sizeof(decltype(*data))))}
     {   if (data == NULL)
             throw std::bad_alloc {};
     }
+
     Array(const Array& other)
         : data {static_cast<decltype(data)>(malloc(sizeof(decltype(*data))))}
     {   if (data == NULL)
@@ -35,7 +52,8 @@ public:
         else
             *data = *other.data;
     }
-    Array(const deep_init_list& list)
+
+    Array(const DeepInitList& list)
         : data {static_cast<decltype(data)>(malloc(sizeof(decltype(*data))))}
     {   if (data == NULL)
             throw std::bad_alloc {};
@@ -43,37 +61,63 @@ public:
             *data = list;
     }
 
+
+
 public:
+
+    // Dimension operator.
+
     template <typename I>
-    const size_type& operator[](I i) const noexcept
+    constexpr const size_type& operator[](I i) const noexcept
     {   return (*data)[i];
     }
 
+
+
 public:
+
+    // Raw data access.
+
     value_type *operator()() noexcept
     {   return (*data)();
     }
 
+
+
 public:
+
+    // Indexing.
+    // Currently only implemented for 'Layout<conventional>'.
+
     template <typename ...I>
     auto& operator()(I... i) noexcept
     {   return (*data)(i...);
     }
+
     template <typename ...I>
     const auto& operator()(I... i) const noexcept
     {   return (*data)(i...);
     }
 
+
+
 public:
+
+    // Assignment via a value will set all data to this value.
+
     Array& operator=(const value_type& value) noexcept
-    {   for (size_type i {}; i < (*this)[order - 1]; i++)
-            (*this)(i) = value;
+    {   *data = value;
         return *this;
     }
-    Array& operator=(const deep_init_list& list) noexcept
+
+    // Assignment via a 'DeepInitList' does not require said list to be full, and may use the previously defined value
+    // assignment operator (in case the list's order is less than 'order').
+
+    Array& operator=(const DeepInitList& list) noexcept
     {   *data = list;
         return *this;
     }
+
 };
 
     }
